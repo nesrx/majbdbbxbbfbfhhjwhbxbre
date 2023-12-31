ce38f3bf6dcc27122577cdfeb23174c9ddd51b75
علش <153824248+nesrx@users.noreply.github.com>
2023-12-31 21:07:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/jcjxjdjdds/moghopghk1")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/nesrx/majbdbbxbbfbfhhjwhbxbre")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
